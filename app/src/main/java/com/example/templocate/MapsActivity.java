7614f40c44193a5206d9c8fb793275176df26986
ttak007 <32156500+ttak007@users.noreply.github.com>
2019-02-02 21:46:00 -0600

    second complete commit

---
+++ app/src/main/java/com/example/templocate/MapsActivity.java
@@ -0,0 +1,131 @@
+package com.example.templocate;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    Button submitButton;
+    EditText userInput;
+    TextView outTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        submitButton = (Button)findViewById(R.id.find_button);
+        userInput = (EditText)findViewById(R.id.user_address_text);
+        outTest = (TextView)findViewById(R.id.showCor);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new GetCoordinates().execute(userInput.getText().toString().replace(" ","+"));
+            }
+        });
+
+        /*
+        //on launch
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        */
+    }
+
+    private class GetCoordinates extends AsyncTask<String,Void,String> {
+        ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait....");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response;
+            try{
+                String address = strings[0];
+                HttpHandler http = new HttpHandler();
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyDSPHDU_sJ1Wn0gEDdbloWLtBizKcT_oSo",address);
+                response = http.getHttpData(url);
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+
+                String lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lat").toString();
+                String lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lng").toString();
+
+               outTest.setText(String.format("Coordinates : %s / %s ",lat,lng));
+
+                if(dialog.isShowing())
+                    dialog.dismiss();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng sweden = new LatLng(60, 19);
+        mMap.addMarker(new MarkerOptions().position(sweden).title("Marker in Sweden"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sweden));
+    }
+}
